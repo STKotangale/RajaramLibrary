b29b781708d64703c9fdc535e1df418700bf324d
prashantri <58700377+prashantri@users.noreply.github.com>
2024-03-30 17:24:37 +0530

    Delete RajaLib directory

--- RajaLib/src/main/java/com/raja/lib/auth/entity/ERole.java
+++
@@ -1,7 +0,0 @@
-package com.raja.lib.auth.entity;
-
-public enum ERole {
-	  ROLE_USER,
-	  ROLE_MODERATOR,
-	  ROLE_ADMIN
-	}
--- RajaLib/src/main/java/com/raja/lib/auth/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.raja.lib.auth.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.raja.lib.auth.entity.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByUsername(String username);
-}
