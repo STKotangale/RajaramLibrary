293af3ae3add6d96f1c6b20e367753f04391e63a
himanshu <himanshudoye42@gmail.com>
2024-04-24 13:32:54 +0530

    added code

--- src/main/java/com/raja/lib/acc/controller/LedgerController.java
+++ src/main/java/com/raja/lib/acc/controller/LedgerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.raja.lib.acc.request.LedgerRequestDTO;
 import com.raja.lib.acc.response.ApiResponseDTO;
 import com.raja.lib.acc.service.LedgerService;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/ledger")
 public class LedgerController {
