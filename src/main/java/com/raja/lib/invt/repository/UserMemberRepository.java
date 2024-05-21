ee4f97950ed9b9ba125e74fdf84eba70f3b9c704
himanshu <himanshudoye42@gmail.com>
2024-05-21 18:10:16 +0530

    chnages in code

--- src/main/java/com/raja/lib/invt/repository/UserMemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.raja.lib.invt.repository;
-
-import com.raja.lib.invt.model.UserMember;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserMemberRepository extends JpaRepository<UserMember, Integer> {
-}
