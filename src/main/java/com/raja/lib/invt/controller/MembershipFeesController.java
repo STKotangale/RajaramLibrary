ed433fb328b3038434011426ea6ae9d4c6d6e798
himanshu <himanshudoye42@gmail.com>
2024-05-25 10:47:04 +0530

    added for member controller

--- src/main/java/com/raja/lib/invt/controller/MembershipFeesController.java
+++ src/main/java/com/raja/lib/invt/controller/MembershipFeesController.java
@@ -3,6 +3,7 @@ package com.raja.lib.invt.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.raja.lib.invt.service.MembershipFeesService;
 
 @RestController
 @RequestMapping("/api/membership-fees")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class MembershipFeesController {
 
     @Autowired
