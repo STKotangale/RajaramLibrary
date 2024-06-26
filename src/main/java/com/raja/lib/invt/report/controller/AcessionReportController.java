0d6fb152025b63e26aca936f2e014d4f5a1db3e0
sandeshkotangale <85024182+sandeshkotangale@users.noreply.github.com>
2024-06-26 11:23:55 +0530

    2024-06-26A

--- src/main/java/com/raja/lib/invt/report/controller/AcessionReportController.java
+++ src/main/java/com/raja/lib/invt/report/controller/AcessionReportController.java
@@ -1,6 +1,8 @@
 package com.raja.lib.invt.report.controller;
 
 import java.io.ByteArrayOutputStream;
+import java.text.ParseException;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -8,7 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,16 +25,27 @@ public class AcessionReportController {
     @Autowired
     private ReportService reportService;
 
-    @GetMapping("/acession")
-    public ResponseEntity<byte[]> getAcessionReport() {
+    @PostMapping("/acession")
+    public ResponseEntity<byte[]> getIssueReport(@RequestBody Map<String, String> dateRange) {
         try {
+            System.out.println("SK1 ");
+               
+            // Generate the report
             ByteArrayOutputStream outputStream = reportService.generateReport();
+            System.out.println("SK2 ");
 
+            // Set the response headers
             HttpHeaders headers = new HttpHeaders();
+            System.out.println("SK3 ");
             headers.setContentType(MediaType.APPLICATION_PDF);
-            headers.add("Content-Disposition", "inline; filename=AcessionReport.pdf");
+            System.out.println("SK4 ");
+            headers.add("Content-Disposition", "inline; filename=IssueReport.pdf");
+            System.out.println("SK5 ");
 
             return new ResponseEntity<>(outputStream.toByteArray(), headers, HttpStatus.OK);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
