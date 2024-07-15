e14374bef05f4f7583629e9c1d4f957108d9fc28
himanshu <himanshudoye42@gmail.com>
2024-07-15 10:36:49 +0530

    added chanages in acession report

--- src/main/java/com/raja/lib/invt/report/service/AcessionReportService.java
+++ src/main/java/com/raja/lib/invt/report/service/AcessionReportService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,6 +20,14 @@ public class AcessionReportService {
         JasperReport jasperReport = JasperCompileManager.compileReport(getClass().getResourceAsStream("/Accession/AcessionReport.jrxml"));
 
         Map<String, Object> parameters = new HashMap<>();
+        InputStream logoStream = this.getClass().getResourceAsStream("/Images/RajaramLogo.png");
+        if (logoStream == null) {
+            logoStream = this.getClass().getResourceAsStream("./Images/RajaramLogo.png");
+            if (logoStream == null) {
+                throw new RuntimeException("Logo image not found in both paths.");
+            }
+        }
+        parameters.put("LOGO_PATH", logoStream);
 
         JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource.getConnection());
 
